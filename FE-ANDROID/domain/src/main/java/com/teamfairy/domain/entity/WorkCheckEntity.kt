476e869e7cd9ca639ee32fc3678c488f58e3a8ca
Sohyun <so405@naver.com>
2024-02-15 07:10:02 +0900

    [UI] : income detail work check ui / #11

---
+++ FE-ANDROID/domain/src/main/java/com/teamfairy/domain/entity/WorkCheckEntity.kt
@@ -0,0 +1,8 @@
+package com.teamfairy.domain.entity
+
+data class WorkCheckEntity(
+    val workCheckId: Int,
+    val daySalary: Int,
+    val workHour: Int,
+    val workDay: String
+)
--- FE-ANDROID/feature/src/main/java/com/teamfairy/feature/util/BindingAdapter.kt
+++ FE-ANDROID/feature/src/main/java/com/teamfairy/feature/util/BindingAdapter.kt
@@ -1,9 +1,14 @@
 package com.teamfairy.feature.util
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
+import com.teamfairy.feature.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String?) {
@@ -16,3 +21,19 @@ fun ImageView.setCircleImage(img: String?) {
         transformations(RoundedCornersTransformation(1000f))
     }
 }
+
+@BindingAdapter("formatDateMMMd")
+fun TextView.formatDateMMMd(date: String) {
+    val formatter =
+        DateTimeFormatter.ofPattern(context.getString(R.string.format_date_MMM_d), Locale.ENGLISH)
+    text = LocalDate.parse(date).format(formatter)
+}
+
+@BindingAdapter("formatDateyyyyMM")
+fun TextView.formatDateyyyyMM(date: String) {
+    val formatter =
+        DateTimeFormatter.ofPattern(context.getString(R.string.format_date_yyyy_MM), Locale.ENGLISH)
+    text = LocalDate.parse(date).format(formatter)
+}
+
+
