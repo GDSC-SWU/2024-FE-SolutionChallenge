8450d4d622438bdedeb2c9f8874eca98ee3385fe
Sohyun <so405@naver.com>
2024-02-15 02:25:19 +0900

    [FEAT] : income recyclerview / #11

---
+++ FE-ANDROID/feature/src/main/java/com/teamfairy/feature/income/IncomeCardAdapter.kt
@@ -0,0 +1,69 @@
+package com.teamfairy.feature.income
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teamfairy.core_ui.view.ItemDiffCallback
+import com.teamfairy.domain.entity.IncomeCardEntity
+import com.teamfairy.feature.databinding.ItemIncomeCardFinishBinding
+import com.teamfairy.feature.databinding.ItemIncomeCardIngBinding
+import com.teamfairy.feature.income.viewholder.IncomeCardFinishViewHolder
+import com.teamfairy.feature.income.viewholder.IncomeCardIngViewHolder
+
+class IncomeCardAdapter(
+    private val onMoveToIncomeDetailClick: (IncomeCardEntity) -> Unit,
+    private val onClickReceiveSalary: (Boolean) -> Unit,
+    private val today: Int
+) : ListAdapter<IncomeCardEntity, RecyclerView.ViewHolder>(
+    IncomeCardDiffCallback
+) {
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getItemViewType(position: Int): Int {
+        return when (today) {
+            currentList[position].payDay.toInt() -> PAY_DAY
+            else -> ING
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            PAY_DAY -> {
+                val binding = ItemIncomeCardFinishBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                IncomeCardFinishViewHolder(binding, onClickReceiveSalary)
+            }
+
+            else -> {
+                val binding = ItemIncomeCardIngBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                IncomeCardIngViewHolder(binding, onMoveToIncomeDetailClick)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (today) {
+            currentList[position].payDay.toInt() -> (holder as IncomeCardFinishViewHolder).run {
+                bind(currentList[position])
+            }
+
+            else -> (holder as IncomeCardIngViewHolder).run {
+                bind(currentList[position])
+            }
+        }
+    }
+
+    companion object {
+        val IncomeCardDiffCallback =
+            ItemDiffCallback<IncomeCardEntity>(onItemsTheSame = { old, new -> old.incomeId == new.incomeId },
+                onContentsTheSame = { old, new -> old == new })
+
+        const val PAY_DAY = 0
+        const val ING = 2
+    }
+}
--- FE-ANDROID/feature/src/main/java/com/teamfairy/feature/income/IncomeFragment.kt
+++ FE-ANDROID/feature/src/main/java/com/teamfairy/feature/income/IncomeFragment.kt
@@ -1,11 +1,34 @@
 package com.teamfairy.feature.income
 
 import com.teamfairy.core_ui.base.BindingFragment
+import com.teamfairy.domain.entity.IncomeCardEntity
 import com.teamfairy.feature.R
 import com.teamfairy.feature.databinding.FragmentIncomeBinding
+import java.time.LocalDate
 
 class IncomeFragment : BindingFragment<FragmentIncomeBinding>(R.layout.fragment_income) {
     override fun initView() {
+        initIncomeCardAdapter()
+    }
 
+    private fun initIncomeCardAdapter() {
+        val list = listOf(
+            IncomeCardEntity(1, "TEST", "15", "1", "5,000"),
+            IncomeCardEntity(1, "TEST", "16", "1", "5,000"),
+            IncomeCardEntity(1, "TEST", "17", "1", "5,000"),
+            IncomeCardEntity(1, "TEST", "18", "1", "5,000")
+        )
+
+        binding.rvIncome.adapter = IncomeCardAdapter(
+            onClickReceiveSalary = {},
+            onMoveToIncomeDetailClick = {},
+            today = dayOfMonth
+        ).apply {
+            submitList(list)
+        }
+    }
+
+    companion object {
+        val dayOfMonth = LocalDate.now().dayOfMonth
     }
 }
